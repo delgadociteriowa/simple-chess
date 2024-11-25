fd33abfa906d1b8534f8bd63d17f7e06f1961c82
Carlos <delgadociteriowa@gmail.com>
2024-11-24 21:36:51 -0300

    base chess state

---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import App from './App';
+
+describe('Simple Chess Board', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it('Shows Simple Chess Board title', () => {
+    const title = screen.getByRole('heading', {
+      name: 'Simple Chess Board',
+    });
+    expect(title).toBeInTheDocument();
+  });
+});
